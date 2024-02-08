f474630ff7d5adcaf8c937dd5d48096a9d69ec36
Emre Can Eski <49168316+nightblabla@users.noreply.github.com>
2024-02-08 08:55:15 +0300

    k

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -171,12 +171,7 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URI,
-      ssl: {
-        rejectUnauthorized: false, // This disables SSL certificate validation
-        // ca: '/path/to/ca-certificate.crt', // Or provide the path to the CA certificate file
-        // Alternatively, you can provide the certificate content directly:
-        // ca: '-----BEGIN CERTIFICATE-----\n...\n-----END CERTIFICATE-----',
-      },
+      
     },
   }),
   collections: [Pages, Posts, Projects, Media, Categories, Users, Comments,Customers,Denemepages,Denememedias],
